9e9cea3c7fd7b0038cdd94ffdb1c4b9a82414a05
Krzysztof Woelke <krzysztof@woelke.pl>
2026-08-22 17:56:00 +0200

    Add multi-channel remote lighting control

--- src/RemoteLightConfig.h
+++ src/RemoteLightConfig.h
@@ -12,12 +12,23 @@ struct RemoteLightConfig
 };
 
 constexpr RemoteLightConfig REMOTE_LIGHTS[] = {
+
+    // Switch na dole przy wyjściu na taras, sterowanie światłem na tarasie(Slave01, CH8)
     {
         InputDevice::MCP1,
         14,
         1, // Slave01
         7  // Child 7 = fizyczny Waveshare CH8
-    }};
+    },
+    {
+        // Switch na dole przy wyjściu głównym, sterowanie lampami chodnik Slave01 CH9){
+        InputDevice::MCP1,
+        4,
+        1, // Slave01
+        16 // Child 8 = Waveshare CH9
+    }
+
+};
 
 constexpr byte REMOTE_LIGHT_COUNT =
     sizeof(REMOTE_LIGHTS) / sizeof(REMOTE_LIGHTS[0]);
